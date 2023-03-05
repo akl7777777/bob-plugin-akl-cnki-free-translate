5adebbb01b5457068455f02b9106c185ed1a65b8
akl7777777 <84266551+akl7777777@users.noreply.github.com>
2023-03-05 17:29:40 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # bob-plugin-akl-cnki-free-translate
-免秘钥CNKI学术翻译Bob插件
+无需apiKey;无需账号密码;免费无限次使用CNKI学术翻译,根据网页版JavaScript加密算法开发的bobplugin;所以只要官网的算法不改,理论上就可以无限使用
+
+
+bob插件主要为bob用户开发,bob是一款macOS上的翻译软件,bob官网地址:https://bobtranslate.com/
+
+#### cnki免秘钥bob插件下载地址:[CNKI学术翻译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-cnki-free-translate/releases/download/v0.1.3/bob-plugin-akl-cnki-translate_v0.1.3.bobplugin)
+
+使用方法:双击安装,直接使用
+
+### bob翻译插件大合集:
+
+
+>[DeepL翻译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-deepl-free-translate)
+
+>[有道翻译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-youdao-free-translate)
+
+>[火山翻译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-volcengine-free-translate)
+
+>[腾讯翻译君插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-tencent-free-translate)
+
+>[腾讯交互翻译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-transmart-free-translate)
+
+>[百度翻译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-baidu-free-translate)
+
+>[彩云小译插件(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-caiyunxiaoyi-free-translate)
+
+>[只为日语 - MOJi辞書（じしょ）](https://github.com/akl7777777/bob-plugin-akl-mojidict-translate)
+
+>[Papago Naver 韩语翻译(免秘钥)](https://github.com/akl7777777/bob-plugin-akl-papago-free-translate)
+
+>[Bob翻译剪切板图片的AlfredWorkflow](https://github.com/akl7777777/BobTranslateClipboard)
+
+>[Bob的Postman接口调试插件](https://github.com/akl7777777/bob-plugin-akl-postman)
+
+
+使用截图如下
+
+<img width="588" alt="image" src="https://user-images.githubusercontent.com/84266551/220933928-52e72e71-8b05-47e8-81a4-323461f0e036.png">
+
+
+### 开发不易,如果喜欢可以请作者喝一杯可乐,谢谢!
+
+
+![image](https://user-images.githubusercontent.com/84266551/219829283-3ed1798e-aeed-4174-bbcb-f93bf3008817.png)
---
+++ src/main.js
@@ -0,0 +1,121 @@
+var CryptoJS = require("crypto-js");
+var config = require('./config.js');
+var utils = require('./utils.js');
+
+function supportLanguages() {
+    return config.supportedLanguages.map(([standardLang]) => standardLang);
+}
+
+function encrypto(t) {
+    const n = '95bae0e3871c9834'
+    const key = CryptoJS.enc.Utf8.parse(n)
+    const encrypted = CryptoJS.AES.encrypt(t, key, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    let result = encrypted.toString().replace(/\//g, '_')
+    result = result.replace(/\+/g, '-')
+    return result
+}
+
+function translate(query, completion) {
+    (async () => {
+        const targetLanguage = utils.langMap.get(query.detectTo);
+        const sourceLanguage = utils.langMap.get(query.detectFrom);
+        if (!targetLanguage) {
+            const err = new Error();
+            Object.assign(err, {
+                _type: 'unsupportLanguage',
+                _message: '不支持该语种',
+            });
+            throw err;
+        }
+
+        const source_lang = sourceLanguage || 'ZH';
+        const target_lang = targetLanguage || 'EN';
+        const translate_text = query.text || '';
+        if (translate_text !== '') {
+            const encryptedText = encrypto(translate_text)
+            const loginUrl = 'https://dict.cnki.net/fyzs-front-api/getToken';
+            const url = 'https://dict.cnki.net/fyzs-front-api/translate/literalTranslation';
+            const header = {
+                'Content-Type': 'application/json;charset=UTF-8',
+                "Referer": "https://dict.cnki.net/index",
+                "Origin": "https://dict.cnki.net",
+                'accept': 'application/json, text/javascript, */*; q=0.01',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                "Token": ""
+            }
+            try {
+                // 获取token
+                const tokenResp = await $http.request({
+                    method: "GET",
+                    url: loginUrl,
+                    header: {
+                        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                    }
+                });
+                if (tokenResp.data && tokenResp.data.code === 200) {
+                    header.Token = tokenResp.data.data
+                } else {
+                    const errMsg = tokenResp.data ? '获取token异常==>' + JSON.stringify(tokenResp.data) : '未知错误'
+                    completion({
+                        error: {
+                            type: 'unknown',
+                            message: errMsg,
+                            addtion: errMsg,
+                        },
+                    });
+                }
+                // 翻译请求
+                const resp = await $http.request({
+                    method: "POST",
+                    url: url,
+                    header: header,
+                    body: {"words": encryptedText, "translateType": null}
+                });
+                $log.error(JSON.stringify(resp))
+                $log.error(JSON.stringify(resp.data))
+                if (resp.data && resp.data.data && resp.data.data.mResult) {
+                    completion({
+                        result: {
+                            from: utils.langMapReverse.get(source_lang),
+                            to: utils.langMapReverse.get(target_lang),
+                            toParagraphs: resp.data.data.mResult.split('\n'),
+                        },
+                    });
+                } else {
+                    const errMsg = '翻译异常请求header==>' + JSON.stringify(header) + '翻译异常请求body参数==>' + JSON.stringify({
+                        "words": encryptedText,
+                        "translateType": null
+                    }) + (resp.data ? '翻译异常==>' + JSON.stringify(resp.data) : '未知错误')
+                    completion({
+                        error: {
+                            type: 'unknown',
+                            message: errMsg,
+                            addtion: errMsg,
+                        },
+                    });
+                }
+            } catch (e) {
+                Object.assign(e, {
+                    _type: 'network',
+                    _message: '接口请求错误 - ' + JSON.stringify(e),
+                });
+                throw e;
+            }
+        }
+    })().catch((err) => {
+        $log.error(err)
+        completion({
+            error: {
+                type: err._type || 'unknown',
+                message: err._message || '未知错误',
+                addtion: err._addtion,
+            },
+        });
+    });
+}
+
+exports.supportLanguages = supportLanguages;
+exports.translate = translate;
